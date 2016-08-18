1a6f14aa7bb711bd0bcd7e0f070ef6ec1977f03c
bearski <santosh.sewlal@gmail.com>
2016-08-18 13:14:49 +0200

    React & Redux #03. Project setup: Hot reloading setup

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,21 @@
 import path from 'path';
+import webpack from 'webpack';
 
 export default {
   devtools: 'eval-source-map',
-  entry: path.join(__dirname, '/client/index.js'),
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/index.js')
+  ],
   output: {
-    path: '/'
+    path: '/',
+    publicPath: '/'
   },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     loaders: [
       {
@@ -13,7 +23,7 @@ export default {
         include: [
           path.join(__dirname, 'client'),
         ],
-        loaders: [ 'babel' ]
+        loaders: [ 'react-hot', 'babel' ]
       }
     ]
   },
