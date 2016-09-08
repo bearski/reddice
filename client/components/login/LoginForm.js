3f4d6a2d62f2a15311fe6940be11a2f116d9d869
bearski <santosh.sewlal@gmail.com>
2016-09-08 13:12:33 +0200

    React & Redux #16. Authentication: Verify User Credentials

--- client/components/login/LoginForm.js
+++ client/components/login/LoginForm.js
@@ -51,7 +51,7 @@ class LoginForm extends React.Component {
       <form onSubmit={this.onSubmit}>
         <h1>Login</h1>
 
-        {/* { errors.form && <div className="alert alert-danger">{errors.form}</div> } */}
+        { errors.form && <div className="alert alert-danger">{errors.form}</div> }
 
         <TextFieldGroup
           field="identifier"
---
+++ server/routes/auth.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+import User from '../models/user';
+import config from '../config';
+
+let router = express.Router();
+
+router.post('/', (req, res) => {
+  const { identifier, password } = req.body;
+
+  User.query({
+    where: { username: identifier },
+    orWhere: { email: identifier }
+  }).fetch().then(user => {
+    if (user) {
+      if (bcrypt.compareSync(password, user.get('password_digest'))) {
+        const token = jwt.sign({
+          id: user.get('id'),
+          username: user.get('username')
+        }, config.jwtSecret);
+        res.json({ token });
+      } else {
+        res.status(401).json({ errors: { form: 'Invalid Credentials' } });
+      }
+    } else {
+      res.status(401).json({ errors: { form: 'Invalid Credentials' } });
+    }
+  });
+});
+
+export default router;
